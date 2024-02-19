c7c4244ee92625410a3bc45aa8a57b644304b222
Jose David Caro Cantor <43614556+josecaro02@users.noreply.github.com>
2024-02-19 20:05:52 +0000

    Add GET, POST, PUT methods

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,71 @@
-import React from "react";
+
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	//logica
+	const [todos, setTodos] = useState([])
+
+	//peticion   resultado   data
+
+	//GET si solo se pone la url en el fetch él asume que es un metodo GET
+	const urlTodos = "https://playground.4geeks.com/apis/fake/todos/user/josecaro02"
+	fetch(urlTodos)
+	.then((response)=>{
+		return response.json()
+	})
+	.then((data)=>{setTodos(data)})
+	.catch((err)=>{return err})
+
+
+	//POST
+	//peticion
+	fetch(urlTodos, {
+		method: "POST",
+		body: JSON.stringify([]),
+		headers: {
+			'Content-Type': 'application/json'
+		}
+	})
+	.then((response)=>{return response.json()})
+	.then((data)=>{console.log(data)})
+	.catch((err)=>{return err})
+
+	//PUT
+	let body = [
+		{
+			done: false,
+			label: "barrer casa"
+		},
+		{
+			done: false,
+			label: "pasear perro"
+		},
+		{
+			done: false,
+			label: "lavar ropa"
+		}
+	]
+	fetch(urlTodos, {
+		method: "PUT",
+		body: JSON.stringify(body),
+		headers: {
+			'Content-Type': 'application/json'
+		}
+	})
+	.then((response)=>{return response.json()})
+	.then((data)=>{console.log(data)})
+	.catch((err)=>{err})
+
+
 	return (
 		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			{todos.map((todos)=>{
+				return <h3>{todos.label}</h3>
+			})}
 		</div>
 	);
 };
