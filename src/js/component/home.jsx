be9f240afd29148738679d603bbd05e77d7fa4c1
Jose David Caro Cantor <43614556+josecaro02@users.noreply.github.com>
2024-02-21 18:38:03 +0000

    ["Add useEffect to avoid multiple reloads"]
    -home.jsx:
        Add useEffect in Get method
        Add useEffect in post method
        Add button to add new tasks

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,5 +1,5 @@
 
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
@@ -8,64 +8,84 @@ import rigoImage from "../../img/rigo-baby.jpg";
 const Home = () => {
 	//logica
 	const [todos, setTodos] = useState([])
-
-	//peticion   resultado   data
-
-	//GET si solo se pone la url en el fetch él asume que es un metodo GET
-	const urlTodos = "https://playground.4geeks.com/apis/fake/todos/user/josecaro02"
-	fetch(urlTodos)
-	.then((response)=>{
-		return response.json()
+	// se ejecuta la accion cada vez que se renderiza el componente
+	/*
+	useEffect(()=>{
+		// se ejecuta una accion
 	})
-	.then((data)=>{setTodos(data)})
-	.catch((err)=>{return err})
+	*/
+
+	//  se ejecuta la accion una sola vez
+	/*
+	useEffect(()=>{
+		// se ejecuta una accion
+	}, [])
+	*/
+
+	// se ejecuta una accion cuando se carga el componente y cada vez que una variable se actualiza
+	/*
+	useEffect(()=>{
+		// se ejecuta accion
+	},[variable1, variable2 , variable3])
+	*/
 
 
+
+	const urlTodos = "https://playground.4geeks.com/apis/fake/todos/user/josecaro02"
 	//POST
 	//peticion
-	fetch(urlTodos, {
-		method: "POST",
-		body: JSON.stringify([]),
-		headers: {
-			'Content-Type': 'application/json'
-		}
-	})
-	.then((response)=>{return response.json()})
-	.then((data)=>{console.log(data)})
-	.catch((err)=>{return err})
+	useEffect(() => {
+		fetch(urlTodos, {
+			method: "POST",
+			body: JSON.stringify([]),
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		})
+			.then((response) => { return response.json() })
+			.then((data) => { console.log(data) })
+			.catch((err) => { return err })
+	}, [])
 
-	//PUT
-	let body = [
-		{
-			done: false,
-			label: "barrer casa"
-		},
-		{
-			done: false,
-			label: "pasear perro"
-		},
-		{
-			done: false,
-			label: "lavar ropa"
+
+	function addNewTask() {
+		let newTodo = {
+			label: "estudiar los hooks",
+			done: false
 		}
-	]
-	fetch(urlTodos, {
-		method: "PUT",
-		body: JSON.stringify(body),
-		headers: {
-			'Content-Type': 'application/json'
-		}
-	})
-	.then((response)=>{return response.json()})
-	.then((data)=>{console.log(data)})
-	.catch((err)=>{err})
+		setTodos([...todos, newTodo])
+		fetch(urlTodos, {
+			method: "PUT",
+			body: JSON.stringify([...todos, newTodo]),
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		})
+			.then((response) => { return response.json() })
+			.then((data) => { console.log(data) })
+			.catch((err) => { err })
+
+	}
+
+	useEffect(() => {
+		// GET Todos
+		//GET si solo se pone la url en el fetch él asume que es un metodo GET
+		fetch(urlTodos)
+			.then((response) => {
+				return response.json()
+			})
+			.then((data) => { setTodos(data) })
+			.catch((err) => { return err })
+	}, [todos])
+
 
 
 	return (
 		<div className="text-center">
-			{todos.map((todos)=>{
+			{todos.map((todos) => {
 				return <h3>{todos.label}</h3>
 			})}
+			<button className="btn btn-success" onClick={addNewTask}>Agregar nueva tarea</button>
 		</div>
 	);
 };
